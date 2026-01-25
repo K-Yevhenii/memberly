9f49632042b77fdddf9e28d6e46e706a691b44e9
Yevhenii Korniakov <ya.korniakov@gmail.com>
2026-01-25 12:43:40 +0200

    add Login component markup

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,12 @@
+import "./Login.scss";
+
+function Login() {
+  return (
+    <div className="auth-buttons text--sm">
+      <button className="auth-buttons__btn">Login</button>
+      <button className="auth-buttons__btn">Sing up</button>
+    </div>
+  );
+}
+
+export default Login;
