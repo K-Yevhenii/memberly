4c954ac2b52c85b03254eeb1c70993d99dde904b
Yevhenii Korniakov <ya.korniakov@gmail.com>
2026-02-25 18:38:10 +0200

    "feat: connect  sections  to App" (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import Header from "./components/Header";
+import Layout from "./components/Layout/Layout";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import ClientsSection from "./components/ClientsSection/ClientsSection";
+import ManagementSection from "./components/ManagementSection/ManagementSection";
 
 function App() {
-  return <Header />;
+  return (
+    <Layout>
+      <Header />
+      <main>
+        <Hero />
+        <ClientsSection />
+        <ManagementSection />
+      </main>
+    </Layout>
+  );
 }
 
 export default App;
